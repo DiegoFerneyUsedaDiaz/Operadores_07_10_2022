9209a6cfb427b0add793a69b3aea8a75a2b7b751
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2022-10-18 15:43:45 -0500

    operador

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("diegoferneyusedadiaz.000webhostapp.com/Operadores_07_10_2022/api.php");
+    let peticion = await fetch("https://diegoferneyusedadiaz.000webhostapp.com/Operadores_07_10_2022/api.php");
     let data = await peticion.text();
     document.querySelector("pre").innerHTML = data;
 }) 
